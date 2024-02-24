21cdc8a496aed307c055d27382096590aa4ec2fa
Kamata954 <kimani.walker954@gmail.com>
2024-02-23 23:19:34 -0500

    updated backend cors

--- db-backend/server.js
+++ db-backend/server.js
@@ -4,7 +4,11 @@ import reviews from "./api/reviews.route.js"
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: ["https://anime-db-six.vercel.app/"],
+    methods: ["POST","GET"],
+    credentials: true
+}))
 app.use(express.json())
 
 app.use("/api/v1/reviews", reviews)
